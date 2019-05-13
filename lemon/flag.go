37412c5d04b5546810dba7cbdff3a8d8f88d5498
max <max@beansmile.com>
2019-05-13 16:46:43 +0800

    update

--- lemon/flag.go
+++ lemon/flag.go
@@ -10,7 +10,9 @@ import (
 	"github.com/monochromegane/conflag"
 )
 
+// parse args from commandline
 func (c *CLI) FlagParse(args []string, skip bool) error {
+
 	style, err := c.getCommandType(args)
 	if err != nil {
 		return err
@@ -22,10 +24,13 @@ func (c *CLI) FlagParse(args []string, skip bool) error {
 	return c.parse(args, skip)
 }
 
+// figure out what command name is using
+// lemonade could be used as a alias or
+// used as a simple command with subcommand
 func (c *CLI) getCommandType(args []string) (s CommandStyle, err error) {
 	s = ALIAS
 	switch {
-	case regexp.MustCompile(`/?xdg-open$`).MatchString(args[0]):
+	case regexp.MustCompile(`/?xdg-open$`).MatchString(args[0]): // 判断是否使用了 alias. 并判断alias的类型.
 		c.Type = OPEN
 		return
 	case regexp.MustCompile(`/?pbpaste$`).MatchString(args[0]):
@@ -37,11 +42,13 @@ func (c *CLI) getCommandType(args []string) (s CommandStyle, err error) {
 	}
 
 	del := func(i int) {
+		// delete specified index argument
 		copy(args[i+1:], args[i+2:])
 		args[len(args)-1] = ""
 	}
 
 	s = SUBCOMMAND
+	// delete subcommand when we know what subcommand it is
 	for i, v := range args[1:] {
 		switch v {
 		case "open":
@@ -66,6 +73,9 @@ func (c *CLI) getCommandType(args []string) (s CommandStyle, err error) {
 	return s, fmt.Errorf("Unknown SubCommand\n\n" + Usage)
 }
 
+// create a FlagSet
+// create flag with default value and description
+// bind flag with struct CLI's field
 func (c *CLI) flags() *flag.FlagSet {
 	flags := flag.NewFlagSet("lemonade", flag.ContinueOnError)
 	flags.IntVar(&c.Port, "port", 2489, "TCP port number")
@@ -80,26 +90,39 @@ func (c *CLI) flags() *flag.FlagSet {
 	return flags
 }
 
+// parse args without subcommand
 func (c *CLI) parse(args []string, skip bool) error {
-	flags := c.flags()
+	flags := c.flags() //初始化一个 FlagSet
 
 	confPath, err := homedir.Expand("~/.config/lemonade.toml")
 	if err == nil && !skip {
+		// 如果配置文件存在的话
+		// 解析配置文件中对应的配置项
+		// 从 配置文件中解析出对应的 args 字符串
 		if confArgs, err := conflag.ArgsFrom(confPath); err == nil {
 			flags.Parse(confArgs)
 		}
 	}
 
+	// if do not have config file
+	// just parse args from command line
 	var arg string
 	err = flags.Parse(args[1:])
 	if err != nil {
 		return err
 	}
+
+	// PASTE and SERVER do not have args
 	if c.Type == PASTE || c.Type == SERVER {
 		return nil
 	}
 
+	// NArg is the number of arguments remaining after flags have been processed.
 	for 0 < flags.NArg() {
+		/* Arg returns the i'th argument. Arg(0) is the first remaining argument after
+		flags have been processed. Arg returns an empty string if the requested element
+		does not exist.
+		*/
 		arg = flags.Arg(0)
 		err := flags.Parse(flags.Args()[1:])
 		if err != nil {
--- lemon/main.go
+++ lemon/main.go
@@ -6,7 +6,12 @@ import (
 	"strings"
 )
 
+// 声明版本的变量
+// 版本变量可以在其他包内访问
 var Version string
+
+// 初始化用法的输出信息
+// 存储了用法信息的变量可以在其他包内访问
 var Usage = fmt.Sprintf(`Usage: lemonade [options]... SUB_COMMAND [arg]
 Sub Commands:
   open [URL]                  Open URL by browser
@@ -29,16 +34,17 @@ Options:
 Version:
   %s`, Version)
 
+// 转换末尾行的结束符
 func ConvertLineEnding(text, option string) string {
 	switch option {
 	case "lf", "LF":
-		text = strings.Replace(text, "\r\n", "\n", -1)
-		return strings.Replace(text, "\r", "\n", -1)
+		text = strings.Replace(text, "\r\n", "\n", -1) // 匹配所有的字符串. 将所有的 `\r\n` 替换为 `\n`
+		return strings.Replace(text, "\r", "\n", -1)   // 匹配所有的字符串. 将所有的 `\r` 替换为 `\n`
 	case "crlf", "CRLF":
-		text = regexp.MustCompile(`\r(.)|\r$`).ReplaceAllString(text, "\r\n$1")
-		text = regexp.MustCompile(`([^\r])\n|^\n`).ReplaceAllString(text, "$1\r\n")
+		text = regexp.MustCompile(`\r(.)|\r$`).ReplaceAllString(text, "\r\n$1")     //构建匹配非 `\r\n` 的正则, 正则表达式必须正确, 否则报错. 通过正则表达式匹配字符串, 并进行替换. 组内的数据作为 $1 传递到新的字符串内.
+		text = regexp.MustCompile(`([^\r])\n|^\n`).ReplaceAllString(text, "$1\r\n") //替换末尾的 `\n` 为 `\r\n`
 		return text
 	default:
-		return text
+		return text // 将处理过的 text 作为默认的输出
 	}
 }
